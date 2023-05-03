c5d976d0758c7d16f66ec22a2e1acf02f38179b1
sejung1218 <sejung1218@hanmail.net>
2023-05-03 16:48:45 +0900

    [Add] install react-query and tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
-'use client';
-import Header from './components/header';
+"use client";
+import Header from "./components/header";
 import React from "react";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
+    // html을 div로 변경하면 hydration 에러가 발생하지 않는다.
+    // commit시 body는 div의 하위 요소로 표시할 수 없다는 에러메시지 출력됨.
     <html lang="en">
-      <header><Header/></header>
+      <Header />
       <body>{children}</body>
     </html>
-  )
+  );
 }
