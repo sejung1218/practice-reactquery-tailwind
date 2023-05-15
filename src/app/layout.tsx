0cf3ca93aede21877b0bfaa5c03c3d3a98f0b8b9
sejung1218 <sejung1218@hanmail.net>
2023-05-15 15:31:48 +0900

    install pakage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ export default function RootLayout({
   return (
     // html을 div로 변경하면 hydration 에러가 발생하지 않는다.
     // commit시 body는 div의 하위 요소로 표시할 수 없다는 에러메시지 출력됨.
-    //
     <html lang="en">
       <body>
         <Header />
